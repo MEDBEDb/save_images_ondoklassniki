9a784383028a339cfc903da7ffbef71985c69814
Sveatoslav Circel <MEDBEDb@me.com>
2016-03-04 17:42:14 +0200

    Fixed script elements according to latest site DOM modifications

--- save_images_ondoklassniki.rb
+++ save_images_ondoklassniki.rb
@@ -22,12 +22,13 @@ end
 def initialize_browser
   @usr = ""
   @pswd = ""
-  @albumURL = "" 
-  #test alubmURL http://www.odnoklassniki.ru/tripdiary/album/52156897755216
-  @b = Watir::Browser.new :firefox #might be :chrome, but worth this one
+  @albumURL = ""
+  @b = Watir::Browser.new :phantomjs #might be :firefox or :chrome, but worth this one
+  @b.window.maximize
+
   if @usr.size>0 && @pswd.size>0 && @albumURL.size>0
     puts "Вы ввели все данные: / Your credentials are set-up: "
-    puts "Пользователь / user: " + @usr + " пароль / password: " + @pswd + " УРЛ / URL: " + @albumURL
+    puts "Пользователь / user: " + @usr + " пароль / password: " + "*****" + " УРЛ / URL: " + @albumURL
   else
     get_user_data
   end
@@ -40,9 +41,9 @@ def login
   @pwd_field = @b.text_field(:id, "field_password")
   @usr_field.when_present.set(@usr)
   @pwd_field.set(@pswd)
-  #@b.send_keys :enter
-  @b.input(:class, "button-pro button-pro_big anonym_login_btn inlineBlock").click
-  @b.span(:id, "portal-headline_login").wait_until_present
+  @b.send_keys :enter
+  @b.span(:class, "mctc_infoContainer_not_block").wait_until_present
+  puts "\n** User Logged In" 
 end
 
 def goto_album
@@ -50,9 +51,8 @@ def goto_album
 end
 
 def get_album_data
-  @total_number_of_photos_in_album = @b.span(:class, 'photo-sc_h2_alb-count portlet-i_h2_tx').text.gsub(/\D/, "").to_i 
+  @total_number_of_photos_in_album = @b.span(:class, 'lstp-t').text.gsub(/\D/, "").to_i 
   @first_image_in_album = @b.img(:class, 'photo-sc_i_cnt_a_img va_target')
-  @main_image = @b.img(:class, "plp_photo") #can be deleted
  end
 
 def delete_a_photo
@@ -63,50 +63,33 @@ def delete_a_photo
 end
 
 def create_album_dirrectory
-  @ablum_title = @b.span(:class, 'photo-sc_h2_alb-title').text.gsub("\d","").gsub("\/"," ").gsub("?"," ").gsub('"','').to_s[0..255]
+  @ablum_title = @b.div(:class, "photo-sc").span(:class, /h_/).text.to_s[0..155].gsub(/:|\w|<|>|\/|(\d|\/|)|\n|'"'/, "") 
   puts "\n** Making directory ./" + @ablum_title + "\n"
   FileUtils.mkdir_p @ablum_title
 end
 
-def save_all_photos_enumerated_with_no_descriptions
-  @first_image_in_album = @b.img(:class, 'photo-sc_i_cnt_a_img va_target')
-  $main_image = @b.img(:class, "plp_photo")
-
-  @first_image_in_album.when_present.click
-  for i in 1..@total_number_of_photos_in_album do
-    sleep 0.2
-    $main_image.wait_until_present
-      File.open(@ablum_title.to_s + "/" + i.to_s + ".jpg", 'wb') do |f|
-      f.write open($main_image.src).read
-      puts "Saving file number " + i.to_s + " and title: " + @ablum_title.to_s
-      end
-  @b.div(:id, "plp_slide_r").when_present.click #clicking next button
-  end
-end
-
-
 def save_all_photos_with_descriptions_def
-@first_image_in_album = @b.img(:class, 'photo-sc_i_cnt_a_img va_target')
-@main_image = @b.img(:class, "plp_photo")
+@first_image_in_album = @b.img(:class, 'va_target')
+@main_image = @b.img(:class, "photo-layer_img rotate__0deg")
 @first_image_in_album.when_present.click
 
 for i in 1..@total_number_of_photos_in_album do
-sleep 0.2
+sleep 0.1
 @main_image.wait_until_present
-@photo_description_title = @b.span(:id => "plp_descrCntText", :class => "plp_descrCntText").text.to_s[0..255].gsub(/(\d|\/|)|\w/, "").gsub("\n","").gsub('"','')
-  if @photo_description_title.size == 0
+@photo_description_title = "" #
+  if @b.span(:id => "plp_descrCntText").exists?
+      @photo_description_title = @b.span(:id => "plp_descrCntText").text.gsub(/:|\w|<|>|\/|(\d|\/|)|'"'/, "")
+      File.open((@ablum_title.to_s + "/" + i.to_s + " " + @photo_description_title).strip.to_s[0..255] + ".jpg", 'wb') do |f|
+      f.write open(@main_image.src).read
+      puts "Saving img " + i.to_s + " and title: " + @photo_description_title
+      end
+  else 
       File.open(@ablum_title.to_s + "/" + i.to_s + " " + @ablum_title.to_s + ".jpg", 'wb') do |f|
       f.write open(@main_image.src).read
       puts "Saving img " + i.to_s + " and title: " + @ablum_title.to_s
       end
-  elsif @photo_description_title.size > 0
-      File.open(@ablum_title.to_s + "/" + i.to_s + " " + @photo_description_title + ".jpg", 'wb') do |f|
-      f.write open(@main_image.src).read
-      puts "Saving img " + i.to_s + " and title: " + @photo_description_title
-      end
   end
- @b.div(:id, "plp_slide_r").hover
- @b.div(:id, "plp_slide_r").when_present.click #clicking next button
+  @b.span(:class => "arw_ic", :index => 1).click
  end
 end
 
@@ -121,7 +104,6 @@ goto_album
 get_album_data
 create_album_dirrectory
 save_all_photos_with_descriptions_def
-#save_all_photos_enumerated_with_no_descriptions
 terminate
 
 #TBU: add captcha finder when opening 200+ photos and decaptcha using tesseract
